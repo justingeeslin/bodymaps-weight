53c1f30b3eed9f74754c89217c3f0b1388816d41
Justin Geeslin <justin@geesl.in>
2021-10-01 20:00:40 -0500

    Enums and the beginning of logic

--- Sources/Hello/main.swift
+++ Sources/Hello/main.swift
@@ -1,23 +1,52 @@
 import ArgumentParser
 
-struct Repeat: ParsableCommand {
-    @Option(help: "Where on the body the augmentation should be placed. forearm")
-    var placement: String?
+// A great way to manage what the string arguments are.. with enums
+enum Placement: String, ExpressibleByArgument {
+    case forearm = "forearm"
+    case waist = "waist"
+    
+}
+
+struct Bodymap: ParsableCommand {
+    @Option(help: "Where on the body the augmentation should be placed.")
+    var placement: Placement?
     
     @Option(help: "Weight of the augmentation in lbs.")
     var weight: Float?
 
     mutating func run() throws {
         let theWeight = weight ?? 50;
-        let thePlacement = placement ?? "forearm";
+        let thePlacement = placement ?? Placement.forearm;
+        
+        switch thePlacement {
+        case .forearm :
+            
+            switch theWeight {
+            case 0..<50:
+                print("Wearable fits on the " + thePlacement.rawValue);
+            default :
+                print("No data supporting placement of the wearable so heavy on that location. Weight: " + String(theWeight) + " Placement: " + thePlacement.rawValue);
+            }
+        case .waist :
+            switch theWeight {
+            case 50:
+                print("Wearable fits on the " + thePlacement.rawValue);
+            default :
+                print("No data supporting placement of the wearable so heavy on that location. Weight: " + String(theWeight) + " Placement: " + thePlacement.rawValue);
+            }
+        default:
+            print("Placement unsupported. Placement " + thePlacement.rawValue);
+            break
+            
+        }
         
         if (theWeight < 0.5) {
-            print("Wearable fits on the " + thePlacement);
+            
         }
         else {
-            print("No data supporting placement of the wearable so heavy on that location. Weight: " + String(theWeight) + " Placement: " + thePlacement);
+            
         }
     }
 }
 
-Repeat.main()
+Bodymap.main()
